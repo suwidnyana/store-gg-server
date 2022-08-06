58395376a17e10bf6ea104141fa4d200729e4aa9
suwidnyanaputra <grayfullbuster522@gmail.com>
2022-08-07 04:40:07 +0800

    add detail page api

--- app/player/controller.js
+++ app/player/controller.js
@@ -13,4 +13,25 @@ module.exports = {
         .json({ message: err.message || `Terjadi kesalahan pada server` });
     }
   },
+  detailPage: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const voucher = await Voucher.findOne({ _id: id })
+        .populate("category")
+        .populate("nominals")
+        .populate("user", "_id name phoneNumber");
+
+      if (!voucher) {
+        return res
+          .status(404)
+          .json({ message: "voucher game tidak ditemukan" });
+      }
+
+      res.status(200).json({ data: voucher });
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: error.message || `Terjadi kesalahan pada server` });
+    }
+  },
 };
