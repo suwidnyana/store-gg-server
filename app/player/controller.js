ba73b0cd72997ec2ae54017d5873358dbeb89190
suwidnyanaputra <grayfullbuster522@gmail.com>
2022-09-07 03:07:15 +0800

    add get category api

--- app/player/controller.js
+++ app/player/controller.js
@@ -1,5 +1,7 @@
 const Player = require("./model");
 const Voucher = require("../voucher/model");
+const Category = require("../category/model");
+
 module.exports = {
   landingPage: async (req, res) => {
     try {
@@ -28,6 +30,16 @@ module.exports = {
       }
 
       res.status(200).json({ data: voucher });
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: error.message || `Terjadi kesalahan pada server` });
+    }
+  },
+  category: async (req, res) => {
+    try {
+      const category = await Category.find();
+      res.status(200).json({ data: category });
     } catch (error) {
       res
         .status(500)
