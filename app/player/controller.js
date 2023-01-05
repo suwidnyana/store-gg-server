739bc2346bfd44280aba475a29418f302029358f
Suwidnyana Putra <grayfullbuster522@gmail.com>
2023-01-05 10:59:22 +0800

    fixing api detail page detail payment section

--- app/player/controller.js
+++ app/player/controller.js
@@ -31,13 +31,15 @@ module.exports = {
         .populate('nominals')
         .populate('user', '_id name phoneNumber');
 
+      const payment = await Payment.find().populate('banks');
+
       if (!voucher) {
         return res
           .status(404)
           .json({ message: 'voucher game tidak ditemukan' });
       }
 
-      res.status(200).json({ data: voucher });
+      res.status(200).json({ data: { detail: voucher, payment } });
     } catch (error) {
       res
         .status(500)
