b9ba3ad36f23db0165570e6350ccbf6ec26a466d
Suwidnyana Putra <grayfullbuster522@gmail.com>
2023-01-11 13:35:37 +0800

    fix email validation error

--- app/player/model.js
+++ app/player/model.js
@@ -1,40 +1,40 @@
-const mongoose = require("mongoose");
-const bcrypt = require("bcryptjs");
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 const HASH_ROUND = 10;
 
 let playerSchema = mongoose.Schema(
   {
     email: {
       type: String,
-      require: [true, "Email harus diisi"],
+      require: [true, 'Email harus diisi'],
     },
     name: {
       type: String,
-      require: [true, "Nama harus diisi"],
-      maxlength: [225, "Panjang nama harus antara 3 - 225 karakter"],
-      minlength: [3, "Panjang nama harus antara 3 - 225 karakter"],
+      require: [true, 'Nama harus diisi'],
+      maxlength: [225, 'Panjang nama harus antara 3 - 225 karakter'],
+      minlength: [3, 'Panjang nama harus antara 3 - 225 karakter'],
     },
     username: {
       type: String,
-      require: [true, "Nama harus diisi"],
-      maxlength: [225, "Panjang username harus antara 3 - 225 karakter"],
-      minlength: [3, "Panjang username harus antara 3 - 225 karakter"],
+      require: [true, 'Nama harus diisi'],
+      maxlength: [225, 'Panjang username harus antara 3 - 225 karakter'],
+      minlength: [3, 'Panjang username harus antara 3 - 225 karakter'],
     },
     password: {
       type: String,
-      require: [true, "Kata sandi harus diisi"],
-      maxlength: [6, "Panjang username harus antara 6 - 225 karakter"],
-      maxlength: [225, "Panjang password maximal 225 karakter"],
+      require: [true, 'Kata sandi harus diisi'],
+      maxlength: [6, 'Panjang username harus antara 6 - 225 karakter'],
+      maxlength: [225, 'Panjang password maximal 225 karakter'],
     },
     role: {
       type: String,
-      enum: ["admin", "user"],
-      default: "user",
+      enum: ['admin', 'user'],
+      default: 'user',
     },
     status: {
       type: String,
-      enum: ["Y", "N"],
-      default: "Y",
+      enum: ['Y', 'N'],
+      default: 'Y',
     },
     avatar: {
       type: String,
@@ -44,30 +44,33 @@ let playerSchema = mongoose.Schema(
     },
     phoneNumber: {
       type: String,
-      require: [true, "Nomor telepon harus diisi"],
-      maxlength: [13, "Panjang nomor telepon harus antara 9 - 13 karakter"],
-      minlength: [9, "Panjang nomor telepon harus antara 9 - 13 karakter"],
+      require: [true, 'Nomor telepon harus diisi'],
+      maxlength: [13, 'Panjang nomor telepon harus antara 9 - 13 karakter'],
+      minlength: [9, 'Panjang nomor telepon harus antara 9 - 13 karakter'],
     },
     favorite: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "Player",
+      ref: 'Player',
     },
   },
   { timestamps: true }
 );
 
-playerSchema.path("email").validate(async function (value) {
-  try {
-    const count = await this.model("Player").countDocuments({ email: value });
-    return !count;
-  } catch (error) {
-    throw error;
-  }
-});
+playerSchema.path('email').validate(
+  async function (value) {
+    try {
+      const count = await this.model('Player').countDocuments({ email: value });
+      return !count;
+    } catch (err) {
+      throw err;
+    }
+  },
+  (attr) => `${attr.value} sudah terdaftar`
+);
 
-playerSchema.pre("save", function (next) {
+playerSchema.pre('save', function (next) {
   this.password = bcrypt.hashSync(this.password, HASH_ROUND);
   next();
 });
 
-module.exports = mongoose.model("Player", playerSchema);
+module.exports = mongoose.model('Player', playerSchema);
