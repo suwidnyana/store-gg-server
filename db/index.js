075caaf814fe9d3c25fa5eb58fc6b173751408d6
Suwidnyana Putra <grayfullbuster522@gmail.com>
2025-07-25 08:15:12 +0800

    update mongoose.set strictquery false

--- db/index.js
+++ db/index.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
 const { urlDb } = require('../config');
 
+
+mongoose.set('strictQuery', false);
 mongoose.connect(urlDb, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
 
 const db = mongoose.connection;
 
